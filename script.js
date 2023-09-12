a570ac1b4ab478b3a1b83d61038748f1dbdb6d14
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2023-09-12 10:41:58 -0300

    Convert money

---
+++ script.js
@@ -0,0 +1,67 @@
+const button = document.getElementById("button");
+const select = document.getElementById("currency-select");
+
+const convertValues = async () => {
+  const inputReais = document.getElementById("input-real").value;
+  const realValueText = document.getElementById("real-value-text");
+  const currencyValueText = document.getElementById("currency-value-text");
+
+  const data = await fetch(
+    "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+  ).then((response) => response.json());
+
+  const dolar = data.USDBRL.high;
+  const euro = data.EURBRL.high;
+  const bitcoin = data.BTCBRL.high;
+
+  realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputReais);
+
+  if (select.value === "US$ Dólar americano") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputReais / dolar);
+  }
+
+  if (select.value === "Є Euro") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputReais / euro);
+  }
+
+  if (select.value === "฿ Bitcoin") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "BTC",
+      minimumFractionDigits: 6,
+    }).format(inputReais / (1000 * bitcoin));
+  }
+};
+
+const changeCurrency = () => {
+  const currencyName = document.getElementById("currency-name");
+  const currencyImg = document.getElementById("currency-img");
+
+  if (select.value === "Є Euro") {
+    currencyName.innerHTML = "Euro";
+    currencyImg.src = "./assets/euro.png";
+  }
+
+  if (select.value === "US$ Dólar americano") {
+    currencyName.innerHTML = "Dólar americano";
+    currencyImg.src = "./assets/eua.png";
+  }
+
+  if (select.value === "฿ Bitcoin") {
+    currencyName.innerHTML = "฿ Bitcoin";
+    currencyImg.src = "./assets/bitcoin.png";
+  }
+  convertValues();
+};
+
+button.addEventListener("click", convertValues);
+select.addEventListener("change", changeCurrency);
